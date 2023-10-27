0ec871defc98b7a7cd5a72987ce773b226dac7ba
ManishW315 <manishdrw1@gmail.com>
2023-10-27 17:07:55 +0530

    Initial project structure.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from setuptools import find_packages, setup
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """Get the requirements/dependencies (packages) in a list."""
+    with open(file_path) as f:
+        lines = f.readlines()
+        requirements = [line.rstrip("\n") for line in lines]
+
+        return requirements
+
+
+setup(
+    name="NewsClassifier",
+    version="1.0",
+    author="ManishW",
+    author_email="manishdrw1@gmail.com",
+    description="",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
